dec5098a7ceae9833f8ab2f30920078015828748
lichen <lidchen@outlook.com>
2024-09-30 08:35:24 +0800

    v1 add basic feature, works fine on obs

--- countdownTimer/countdownTimer.js
+++ countdownTimer/countdownTimer.js
@@ -1,9 +1,25 @@
 let countdown; //countdown as a global variable
+const displayElement = document.querySelector('#timer');
+const startButton = document.querySelector('#startButton');
+const input = document.querySelector('#minutes')
+const controlContainer = document.querySelector('#controlContainer');
+
+
+function soundPlant() {
+    var sound = document.getElementById("c4Plant");
+    sound.play().catch(e => console.log("Error playing sound:", e));
+}
+
+function soundExplode() {
+    var sound = document.getElementById("c4Explode");
+    sound.play();
+}
 
 function countdownTimer (duration,displayElement) {
     if (countdown) {
         clearInterval(countdown);
     }
+    controlContainer.style.display = 'none';
     let timeremain = duration, minutes, seconds;
     countdown = setInterval(
     function(){
@@ -15,20 +31,26 @@ function countdownTimer (duration,displayElement) {
 
         displayElement.textContent = minutes + ":" + seconds;
 
+        if (timeremain === 35){
+            soundExplode();
+        }
+
         if (--timeremain < 0){
             clearInterval(countdown);
-            displayElement.textContent = "times up";
+            displayElement.textContent = "EEEE";
+            controlContainer.style.display = 'block';
         }
     }, 1000);
 }
 
-const displayElement = document.querySelector('#timer');
-const startButton = document.querySelector('#startButton');
-const input = document.querySelector('#minutes')
+
 
 startButton.addEventListener('click', function() {
+    soundPlant();
     let minutes = parseInt(input.value, 10);
     if (minutes > 99) { minutes = 99; input.value = 99;}
     const seconds = minutes * 60;
-    countdownTimer(seconds, displayElement)}
-);
+    countdownTimer(seconds, displayElement)
+});
+
+controlContainer.style.display = 'block';
